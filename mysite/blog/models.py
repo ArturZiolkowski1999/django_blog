ae97688d5378305694d522e184c43f00d3784164
Artur Ziółkowski <aaartiii99@gmail.com>
2022-02-22 14:48:31 +0100

    create models, and view function of simple blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -23,3 +31,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_ulr(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
